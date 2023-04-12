904e656847512848404aeb2679ad815eb6fe9fd6
Victor Online <victorbellasartes@gmail.com>
2023-04-12 18:49:55 -0500

    actualizando

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+
 // Syncing all the models at once.
 conn.sync({ force: false}).then( async () => {
   //await saveApi();
