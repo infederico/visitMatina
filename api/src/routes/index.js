59bde2d418f4261346c8ef50a6e199e3b89847c4
Noe Alonso Rabadan <nalonsor@outlook.com>
2023-04-13 12:35:15 -0600

    Rutas para Media

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,10 @@
 const { Router } = require('express');
 
+//Media
+const { media } = require("./media/media")
+
 const router = Router();
+
+router.use('/media', media);
 
 module.exports = router;
