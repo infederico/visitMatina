ad8aef33478dd3352c287c7fbe040ab335056e4b
infederico <infederico@gmail.com>
2023-04-14 20:12:11 -0300

    Reviews - ya puede ordenar filtrar y paginar

--- client/src/components/common/Reviews/Reviews.jsx
+++ client/src/components/common/Reviews/Reviews.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 import { useLocation } from 'react-router-dom';
 
-import Review from './Review/Review';
+import Review from './Review/Review'; // componente Review (card)
 
 import styles from './Reviews.module.css';
 
@@ -14,19 +14,21 @@ const Reviews = () => {
     const reviews = apiRes.response;
 
     // local states
-    const [ overallRatingWord, setOverallRatingWord ] = useState('');
     const [ overallRatingNumber, setOverallRatingNumber ] = useState(0);
-
+    const [ overallRatingWord, setOverallRatingWord ] = useState('');
     const [ newReview, setNewReview ] = useState({
         name: '',
         email: '',
-        rating: 5,
+        rating: 5, // por defecto esta seteado en la maxima calificacion
         description: ''
     });
-
     const [ filteredReviews, setFilteredReviews ] = useState([]);
+    const [ filterSelectedOption, setFilterSelectedOption ] = useState('all'); // por defecto se inicializa el filtro en "mostrar: Todas"
     const [ sortedReviews, setSortedReviews ] = useState([]);
+    const [ sortSelectedOption, setSortSelectedOption ] = useState(''); // por defecto se inicializa el ordenamiento en "ordenar: " (no sort)
     const [ paginatedReviews, setPaginatedReviews ] = useState([]);
+    const [ currentPage, setCurrentPage ] = useState(1);
+    
 
     const [ errors, setErrors ] = useState({});
 
@@ -70,15 +72,18 @@ const Reviews = () => {
         // lo redondeo para que vaya de 0.5 en 0.5
         let roundedOverallRating = Math.round(quotient * 2) / 2;
         setOverallRatingNumber(roundedOverallRating);
-    }, [reviews]);
+    }, [reviews, filterSelectedOption]);
 
     const ratingScale = {
-        0: 'PESIMO',
+        // NEGATIVAS
+        0: 'MUY MALO',
         0.5: 'MUY MALO',
-        1: 'MUY MALO',
+        1: 'MALO',
         1.5: 'MALO',
-        2: 'MALO',
-        2.5: 'MALO',
+        //NEUTRALES
+        2: 'REGULAR',
+        2.5: 'REGULAR',
+        // POSITIVAS
         3: 'BUENO',
         3.5: 'BUENO',
         4: 'MUY BUENO',
@@ -89,17 +94,47 @@ const Reviews = () => {
         setOverallRatingWord(ratingScale[overallRatingNumber]);
     }, [overallRatingNumber]);
     
-    useEffect( () => { // FALTA LA LOGICA PARA FILTRAR
-        setFilteredReviews(reviews);
-    }, [reviews]);
+    // LOGICA PARA FILTRAR
+    useEffect( () => { 
+        let filteredByRating = [];
+        switch (filterSelectedOption) {
+            case 'all':
+                filteredByRating = reviews;
+                break;
+            case 'positives':
+                filteredByRating = reviews.filter((review) => review.rating >= 3);
+                break;
+            case 'negatives':
+                filteredByRating = reviews.filter((review) => review.rating < 3);
+                break;
+            default:
+                filteredByRating = reviews;
+                break;
+        };
+        setFilteredReviews(filteredByRating);
+        setCurrentPage(1);
+    }, [reviews, filterSelectedOption]);
 
-    useEffect( () => { // FALTA LA LOGICA PARA ORDENAR
-        setSortedReviews(filteredReviews);
-    }, [filteredReviews]);
+    // LOGICA PARA ORDENAR
+    // functions with each sorting option logic - mapped to an object for cleaner code
+    const sortFunctions = {
+        'rating-asc': (a, b) => a.rating - b.rating,
+        'rating-des': (a, b) => b.rating - a.rating,
+        'date-asc': (a, b) => new Date(a.date.split('/').reverse().join('-')) - new Date(b.date.split('/').reverse().join('-')),
+        'date-des': (a, b) => new Date(b.date.split('/').reverse().join('-')) - new Date(a.date.split('/').reverse().join('-')),
+    };
+    useEffect( () => { 
+        let auxSortedReviews = sortSelectedOption ? filteredReviews.slice().sort(sortFunctions[sortSelectedOption]) : filteredReviews;
+        setSortedReviews(auxSortedReviews);
+        setCurrentPage(1);
+    }, [filteredReviews, sortSelectedOption]);
     
-    useEffect( () => { // FALTA LA LOGICAPAGINAR
-        setPaginatedReviews(sortedReviews);
-    }, [sortedReviews]);
+
+    // LOGICA PARA PAGINAR
+    useEffect( () => { 
+        let auxPaginatedReviews = sortedReviews.slice(((currentPage * 6) - 6), (currentPage * 6))
+        setPaginatedReviews(auxPaginatedReviews);
+    }, [sortedReviews, currentPage]);
     
 
     // handlers
@@ -119,6 +154,31 @@ const Reviews = () => {
         }
     };
 
+    // selector de filtrado
+    const handleFilterChange = (event) => {
+        setFilterSelectedOption(event.target.value);
+    };
+    
+    const handleSortChange = (event) => {
+        setSortSelectedOption(event.target.value);
+    };
+
+    const pageIncrement = () => {
+        let lastPage = Math.ceil(sortedReviews.length / 6);
+        if (currentPage < lastPage) {
+            let nextPage = currentPage + 1;
+            setCurrentPage(nextPage);
+        }
+    };
+
+
+    const pageDecrement = () => {
+        if (1 < currentPage) {
+            let prevPage = currentPage - 1;
+            setCurrentPage(prevPage);
+        }
+    };
+
     const handleSubmit = (event) => {
         event.preventDefault();
     };
@@ -131,11 +191,39 @@ const Reviews = () => {
             </div>
             <hr />
             <div>
+                <span>calificación </span>
                 <span>{` ${overallRatingWord} - `}</span> 
                 <span>{` ${overallRatingNumber} - `}</span>
                 <span>{` ${reviews.length} reseñas `}</span>
             </div>
             <hr />
+
+            <span>
+                <label htmlFor="rating-filter">mostrar:  </label>
+                <select id="rating-filter" value={filterSelectedOption} onChange={handleFilterChange} className={styles.select} style={{outline: "none"}}> 
+                    <option value="all">Todas</option>
+                    <option value="positives">Positivas</option>
+                    <option value="negatives">Negativas</option>
+                </select>
+            </span>
+                <div className={styles.pagination}>
+                    <button onClick={pageDecrement}>prev</button>
+                    <span>{`Page ${currentPage}`}</span>
+                    <button onClick={pageIncrement}>next</button>
+                </div>
+            <span>
+
+            </span>
+
+            <span>
+                <label htmlFor='sort'>ordenar por:  </label>
+                <select id='sort' value={sortSelectedOption} onChange={handleSortChange} className={styles.select} style={{outline: "none"}}> 
+                    <option value='date-des'>Fecha - Más recientes primero</option>
+                    <option value='date-asc'>Fecha - Más antiguas primero</option>
+                    <option value='rating-des'>Mejor puntuación - des.</option>
+                    <option value='rating-asc'>Peor puntuación - asc.</option>
+                </select>
+            </span>
             <div className={styles.reviewCards}>
                 {
                     paginatedReviews?.map((review) => {
@@ -151,6 +239,7 @@ const Reviews = () => {
                 }
             </div>
             <hr />
+
             <div>
                 <span>Deja tu reseña...</span>
                 <br />
@@ -164,7 +253,7 @@ const Reviews = () => {
                     <br />
 
                     <label>Email: </label>
-                    <input type='text' name='email' onChange={handleChange} value={newReview.email} style={{outline: "none"}} placeholder='optional'/>
+                    <input type='text' name='email' onChange={handleChange} value={newReview.email} style={{outline: "none"}} placeholder='opcional'/>
                     {errors.email && <span className={styles.errors} >{errors.email}</span>}
                     <br />
                     <br />
@@ -198,4 +287,4 @@ const Reviews = () => {
     );
 };
 
-export default Reviews;
+export default Reviews; //
