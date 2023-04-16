fbc536b63da93a86d0155845eb0054fd8a34e432
Victor Online <victorbellasartes@gmail.com>
2023-04-15 19:11:01 -0500

    paginasshopsmaqueta

--- client/src/components/pages/FincaLaParcela/descriptions.js
+++ client/src/components/pages/FincaLaParcela/descriptions.js
@@ -1,13 +1,13 @@
 
 
 
-export const descriptions = `Claro de Luna es un pequeño hospedaje familiar 
+export const descriptions = `Finca La Parcela es un pequeño hospedaje familiar 
 situado en una zona residencial tranquila. Si buscas un lugar acogedor y cómodo 
 para tu estancia, este es el lugar ideal. Ofrecemos habitaciones equipadas con 
 aire acondicionado, baño privado y televisión por cable para que puedas disfrutar 
 de tu privacidad y comodidad. Además, nuestro servicio de limpieza diario asegura 
 que tu habitación siempre esté en perfecto estado.
-En Claro de Luna, no solo nos preocupamos por la comodidad de nuestros huéspedes, 
+En Finca La Parcela, no solo nos preocupamos por la comodidad de nuestros huéspedes, 
 sino también por hacer que su estancia sea lo más agradable posible. Nuestra 
 familia está siempre disponible para ayudar con recomendaciones locales y consejos
  para que puedas disfrutar de la zona al máximo. Además, nuestro delicioso desayuno 
@@ -17,5 +17,5 @@ Si necesitas trabajar o mantenerte conectado, nuestro Wi-Fi gratuito es rápido 
  relajarte en nuestra zona de descanso al aire libre y disfrutar del ambiente tranquilo y 
  agradable.`;
 
- export const name = "Hospedaje Claro de Luna";
- export const imagen = 'https://www.livevalledelcauca.com/assets2018/images/calima-darien/finca-campestre-heidy-1.jpg'
+ export const name = "Finca La Parcela";
+ export const imagen = 'https://imgcy.trivago.com/c_fill,d_dummy.jpeg,e_sharpen:60,f_auto,h_627,q_auto,w_1200/itemimages/31/92/3192963_v1.jpeg'
--- client/src/components/pages/FincaMandira/descriptions.js
+++ client/src/components/pages/FincaMandira/descriptions.js
@@ -1,13 +1,13 @@
 
 
 
-export const descriptions = `Claro de Luna es un pequeño hospedaje familiar 
+export const descriptions = `Finca Mandira es un pequeño hospedaje familiar 
 situado en una zona residencial tranquila. Si buscas un lugar acogedor y cómodo 
 para tu estancia, este es el lugar ideal. Ofrecemos habitaciones equipadas con 
 aire acondicionado, baño privado y televisión por cable para que puedas disfrutar 
 de tu privacidad y comodidad. Además, nuestro servicio de limpieza diario asegura 
 que tu habitación siempre esté en perfecto estado.
-En Claro de Luna, no solo nos preocupamos por la comodidad de nuestros huéspedes, 
+En Finca Mandira, no solo nos preocupamos por la comodidad de nuestros huéspedes, 
 sino también por hacer que su estancia sea lo más agradable posible. Nuestra 
 familia está siempre disponible para ayudar con recomendaciones locales y consejos
  para que puedas disfrutar de la zona al máximo. Además, nuestro delicioso desayuno 
@@ -17,5 +17,5 @@ Si necesitas trabajar o mantenerte conectado, nuestro Wi-Fi gratuito es rápido 
  relajarte en nuestra zona de descanso al aire libre y disfrutar del ambiente tranquilo y 
  agradable.`;
 
- export const name = "Hospedaje Claro de Luna";
- export const imagen = 'https://www.livevalledelcauca.com/assets2018/images/calima-darien/finca-campestre-heidy-1.jpg'
+ export const name = "Finca Mandira";
+ export const imagen = 'https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/326131398.jpg?k=4e8e302b83d5abe7b5257f18a47b5c56da18b7e8629a1ca446f6fdda9c8d2f5a&o='
