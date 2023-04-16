4023db9fa4610e5ec0cfde7e3dbc2468660fd4b5
LisandroAreco <areco.lisandro@gmail.com>
2023-04-16 17:01:48 -0300

    Creada Galeria para el Restaurant

--- client/src/components/pages/RestaurantSolYLuna/RestaurantSolYLuna.jsx
+++ client/src/components/pages/RestaurantSolYLuna/RestaurantSolYLuna.jsx
@@ -1,7 +1,22 @@
+import styles from "./RestaurantSolYLuna.module.css"
+
 export default function RestauranteSolYLuna() {
+    
+    let DB = require("./imagenes.json")
+    DB = DB.response
+    
+    
     return(
         <>
-            <h1>Restaurante</h1>       
+        {console.log(DB)}
+            <h1>Restaurante Sol y Luna</h1>
+            <div className={styles.gallery}>
+                {DB.map(image => {
+                    return(
+                        <img src={image.img} alt="AGREGAR ALT" />
+                    )
+                })}
+            </div>       
         </>
     )
 }
