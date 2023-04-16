721c74d64976a43e0aaea8fded6f85d02e0b994d
Noe Alonso Rabadan <nalonsor@outlook.com>
2023-04-16 15:33:35 -0600

    about y paquetes

--- client/src/components/pages/AboutUs/AboutUs.jsx
+++ client/src/components/pages/AboutUs/AboutUs.jsx
@@ -1,7 +1,10 @@
+import Carousel from '../../common/Carousel/Carousel';
+
 export default function AboutUs() {
     return(
         <>
             <h1>Nosotros</h1>       
+            <Carousel />
         </>
     )
 }
