0b1a606d1cf180d8f6f673bb2d73dbb4e03b078c
leoncaal <leonel_1mx@yahoo.com.mx>
2023-04-16 13:21:29 -0600

    Agregue a DetailBlog el componente ReviewForm

--- client/src/components/common/CardBlogDetail/CardBlogDetail.jsx
+++ client/src/components/common/CardBlogDetail/CardBlogDetail.jsx
@@ -1,18 +1,22 @@
 import style from './CardBlogDetail.module.css';
+import ReviewForm from '../../common/Reviews/ReviewForm/ReviewForm';
 
 const CardBlogDetail = (props) => {
     return (
+      <div>
         <div className={`${style.card} card mb-3`}>
             <h5 className={`${style.nombre} card-title`}>{props.name}</h5>
-          <div className="card-body">
+          <div className={`card-body`}>
             <img className={`${style.foto} card-img-top`} src={props.image} alt="..." />
             <p className={`${style.descri} card-text`}>{props.content}</p>
-            <p className="card-text">
-              <small className="text-body-secondary">
-              <p className={`${style.descri} card-text`}>{props.date}</p>
-              </small>
-            </p>
+  
+              <p className={`${style.descri} card-text`}>Fecha de creación: {props.date}</p>
+
           </div>
+        </div>
+        <div>
+          <ReviewForm></ReviewForm>
+        </div>
         </div>
     );
   };
