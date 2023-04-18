dfc5c4281541c7f75957ce9f810f4d3f33d5c6c9
LisandroAreco <areco.lisandro@gmail.com>
2023-04-18 12:04:49 -0300

    pre merging por toolkit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,15 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import reducer from './reducer';
+import { configureStore } from "@reduxjs/toolkit";
 
-const composeEnhancers =
-   (typeof window !== 'undefined' &&
-      window.REDUX_DEVTOOLS_EXTENSION_COMPOSE) ||
-   compose;
-
-const store = createStore(
-   reducer,
-   composeEnhancers(applyMiddleware(thunk)),
-);
-
-export default store;
+export default configureStore({
+   reducer: {
+      productos: productos
+   }
+})
