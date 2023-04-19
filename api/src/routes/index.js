c963eb1011946210b59080473024ef8bf9c1e15a
jose <joseerahe@gmail.com>
2023-04-18 18:57:35 -0600

    user

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,22 +1,20 @@
 const { Router } = require('express')
 const usersRouter = require('../routes/users/usersRouter')
 const productRouter = require('../routes/product/productRouter')
-const { media } = require("./media/media")
-const post = require("./post/post");
-const shopsRoutes = require("./shops/shopsRoutes");
+const { media } = require('./media/media')
+const post = require('./post/post')
+const shopsRoutes = require('./shops/shopsRoutes')
 
 const router = Router()
 
-router.use('/user', usersRouter)
+router.use('/users', usersRouter)
 router.use('/product', productRouter)
 
 //Media
-router.use('/media', media);
+router.use('/media', media)
 
-router.use('/post', post);
+router.use('/post', post)
 
+router.use('/shops', shopsRoutes)
 
-router.use('/shops',shopsRoutes);
-
-
-module.exports = router;
+module.exports = router
