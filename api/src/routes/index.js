d0ac611698f9335853d4e3cbb310f4f0334fe032
jose <joseerahe@gmail.com>
2023-04-22 15:22:55 -0600

    back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 const usersRouter = require('../routes/users/usersRouter')
 const productRouter = require('../routes/product/productRouter')
 
-const { media } = require('./media/media')
+// const { media } = require('./media/media')
 const post = require('./post/post')
 const shopsRoutes = require('./shops/shopsRoutes')
 const { comment } = require('./comments/comments')
@@ -15,7 +15,7 @@ router.use('/users', usersRouter)
 router.use('/product', productRouter)
 
 //Media
-router.use('/media', media)
+// router.use('/media', media)
 router.use('/comments', comment)
 router.use('/reviews', review)
 
