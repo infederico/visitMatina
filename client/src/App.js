18746b0ea4ad411edd16c0e665323feda78df951
LisandroAreco <areco.lisandro@gmail.com>
2023-04-23 22:30:07 -0300

    prueba preparada para onrender

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ import Admin from './components/pages/Admin/Admin'
 
 import axios from 'axios'
 // axios.defaults.baseURL = 'http://localhost:3001/api'
-axios.defaults.baseURL = 'https://visitmatina-production.up.railway.app'
+axios.defaults.baseURL = 'https://visitmatina.onrender.com/api'
 
 const App = () => {
   return (
