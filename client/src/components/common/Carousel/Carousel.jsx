9ad54dc35cefa3f7214b5a83b13b9f4c2f27cb67
LisandroAreco <areco.lisandro@gmail.com>
2023-04-28 19:32:55 -0300

    carousel y aboutUs terminado

--- client/src/components/common/Carousel/Carousel.jsx
+++ client/src/components/common/Carousel/Carousel.jsx
@@ -5,29 +5,27 @@ const Carousel = () =>{
     return(
 
         <>
-        <div id="carouselExampleFade" className="carousel slide carousel-fade">
+        <div id="carouselExampleFade" className={`carousel slide carousel-fade h-50 ${styles.container}`}>
 
 
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                    <img src="https://fondosmil.com/fondo/74416.jpg" className="d-block w-100" alt="..."/>
+                    <img src="https://res.cloudinary.com/dfnw2l08x/image/upload/v1682718864/carousel5_brudp0.jpg" className={`d-block w-100 ${styles.img}`} alt="..."/>
                     <div className={`d-none d-lg-block  carousel-caption ${styles.customCaption}`}>
-                        <h4>Hola mundo</h4>
-                        <p>Somos un grupo de emprendedores turísticos que nos aliamos para presentarte una cara de Matina que no has visto: Una tierra verde, abundante, productiva y con una belleza escénica impresionante.</p>                     
+                        {/* <h4 className={styles.title}>¡Hola a todos!</h4> */}
+                        <p className={styles.body}>Somos un grupo de emprendedores turísticos que nos aliamos para presentarte una cara de Matina que no has visto: Una tierra verde, abundante, productiva y con una belleza escénica impresionante.</p>                     
                     </div>
                 </div>
                 <div className="carousel-item">
-                    <img src="https://i.ytimg.com/vi/kcBQlmcNuJQ/maxresdefault.jpg" className="d-block w-100" alt="..."/>
+                    <img src="https://res.cloudinary.com/dfnw2l08x/image/upload/v1682718080/carousel2_vn2laz.jpg" className={`d-block w-100 ${styles.img}`} alt="..."/>
                     <div className={`d-none d-lg-block carousel-caption ${styles.customCaption} `}>
-                        <h4>Lorem</h4>
-                        <p>Como familias buscamos aportar al desarrollo local promoviendo un turismo responsable y sostenible, valorando y respetando la cultura, la historia y la biodiversidad.</p>
+                        <p className={styles.body}>Como familias buscamos aportar al desarrollo local promoviendo un turismo responsable y sostenible, valorando y respetando la cultura, la historia y la biodiversidad.</p>
                     </div>
                 </div>
                 <div className="carousel-item">
-                    <img src="https://www.diarioextra.com/files/Dnews/images/detail/486531_ninodesapareceenrio.jpg" className="d-block w-100" alt="..."/>
+                    <img src="https://res.cloudinary.com/dfnw2l08x/image/upload/v1682717908/carousel1_ctvqae.jpg" className={`d-block w-100 ${styles.img}`} alt="..."/>
                     <div className={`d-none d-lg-block  carousel-caption ${styles.customCaption}`}>
-                        <h4>Aenean sodales aliquet ante</h4>
-                        <p>Si buscas una experiencia auténtica, personalizada y sostenible en el cantón de Matina, ¡estamos listos para recibirte! Contáctanos y permítenos diseñar una experiencia inolvidable que se adapte a tus intereses y necesidades. ¡Te esperamos en nuestra hermosa región!</p>                    
+                        <p className={styles.body}>Si buscas una experiencia auténtica, personalizada y sostenible en el cantón de Matina, ¡estamos listos para recibirte! Contáctanos y permítenos diseñar una experiencia inolvidable que se adapte a tus intereses y necesidades. ¡Te esperamos en nuestra hermosa región!</p>                    
                     </div>
                 </div>
             </div>
