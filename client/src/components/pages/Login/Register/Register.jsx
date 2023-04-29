7309b73ac1dbcb159303cd81361510fadb800d35
jose <joseerahe@gmail.com>
2023-04-29 17:19:31 -0600

    arreglado register y login

--- client/src/components/pages/Login/Register/Register.jsx
+++ client/src/components/pages/Login/Register/Register.jsx
@@ -52,140 +52,123 @@ const Register = () => {
     <div>
       <img
         src='https://res.cloudinary.com/dfnw2l08x/image/upload/v1682798064/fondoregistroreal_s6icep.jpg'
-        classNameName={` ${styles.backgroundImage} img-fluid`}
+        className={` ${styles.backgroundImage} img-fluid`}
         alt='...'
         style={{ width: '33%', marginLeft: '.5%' }}
       />
       <img
         src='https://res.cloudinary.com/dfnw2l08x/image/upload/v1682798064/fondoregistroreal_s6icep.jpg'
-        classNameName={` ${styles.backgroundImage} img-fluid`}
+        className={` ${styles.backgroundImage} img-fluid`}
         alt='...'
         style={{ width: '33%' }}
       />
       <img
         src='https://res.cloudinary.com/dfnw2l08x/image/upload/v1682798064/fondoregistroreal_s6icep.jpg'
-        classNameName={` ${styles.backgroundImage} img-fluid`}
+        className={` ${styles.backgroundImage} img-fluid`}
         alt='...'
         style={{ width: '33%' }}
       />
-      <div classNameName='card-body p-4'>
-        <div classNameName={`card ${styles.cardContainer}`}>
-          <h2 classNameName={`text-center mb-4 ${styles.registrationTitle}`}>
-            Registro
-          </h2>
+      <div className='card-body p-4'></div>
+      <div className={`card ${styles.cardContainer}`}>
+        <h2 className={`text-center mb-4 ${styles.registrationTitle}`}>
+          Registro
+        </h2>
+        <br />
+        <form>
+          <div class='form-group'>
+            <label for='name' className={styles.registrationLabel}></label>
+            <input
+              class='form-control'
+              id='name'
+              placeholder='Nombre'
+              name='name'
+              type='text'
+              value={userData.name}
+              onChange={handleInputChange}
+              className={styles.registrationInput}
+            />
+            {errors.name ? (
+              <p className={styles.registrationLabel}>{errors.name}</p>
+            ) : null}
+          </div>
           <br />
-          <form>
-            <div className='form-group'>
-              <label
-                htmlFor='name'
-                classNameName={styles.registrationLabel}
-              ></label>
-              <input
-                className='form-control'
-                id='name'
-                placeholder='Nombre'
-                name='name'
-                type='text'
-                value={userData.name}
-                onChange={handleInputChange}
-                classNameName={styles.registrationInput}
-              />
-              {errors.name ? (
-                <p classNameName={styles.registrationLabel}>{errors.name}</p>
-              ) : null}
-            </div>
+          <br />
+          <div class='form-group'>
+            <label for='email' className={styles.registrationLabel}></label>
+            <input
+              type='email'
+              class='form-control'
+              id='email'
+              placeholder='Correo electrónico'
+              name='email'
+              value={userData.email}
+              onChange={handleInputChange}
+              className={styles.registrationInput}
+            />
+            {errors.email ? (
+              <p className={styles.registrationLabel}>{errors.email}</p>
+            ) : null}
             <br />
+          </div>
+          <div class='form-group'>
+            <label for='email' className={styles.registrationLabel}></label>
+            <input
+              class='form-control'
+              id='email'
+              placeholder='Confirma Correo electrónico'
+              type='email'
+              name='confirmEmail'
+              value={userData.confirmEmail}
+              onChange={handleInputChange}
+              className={styles.registrationInput}
+            />
+            {errors.confirmEmail ? (
+              <p className={styles.registrationLabel}>{errors.confirmEmail}</p>
+            ) : null}
+          </div>
+          <br />
+          <div class='form-group'>
+            <label for='password' className={styles.registrationLabel}></label>
+            <input
+              class='form-control'
+              id='password'
+              placeholder='Contraseña'
+              name='password'
+              type='password'
+              value={userData.password}
+              onChange={handleInputChange}
+              className={styles.registrationInput}
+            />
+            {errors.password ? (
+              <p className={styles.registrationLabel}>{errors.password}</p>
+            ) : null}
+          </div>
+          <br />
+          <div class='form-group'>
+            <label className={styles.registrationLabel}></label>
+            <input
+              name='confirmPassword'
+              placeholder='Confirma Contraseña'
+              type='password'
+              value={userData.confirmPassword}
+              onChange={handleInputChange}
+              className={styles.registrationInput}
+            />
+            {errors.confirmPassword ? (
+              <p className={styles.registrationLabel}>
+                {errors.confirmPassword}
+              </p>
+            ) : null}
             <br />
-            <div className='form-group'>
-              <label
-                htmlFor='email'
-                classNameName={styles.registrationLabel}
-              ></label>
-              <input
-                type='email'
-                className='form-control'
-                id='email'
-                placeholder='Correo electrónico'
-                name='email'
-                value={userData.email}
-                onChange={handleInputChange}
-                classNameName={styles.registrationInput}
-              />
-              {errors.email ? (
-                <p classNameName={styles.registrationLabel}>{errors.email}</p>
-              ) : null}
-              <br />
-            </div>
-            <div className='form-group'>
-              <label
-                htmlFor='email'
-                classNameName={styles.registrationLabel}
-              ></label>
-              <input
-                className='form-control'
-                id='email'
-                placeholder='Confirma Correo electrónico'
-                type='email'
-                name='confirmEmail'
-                value={userData.confirmEmail}
-                onChange={handleInputChange}
-                classNameName={styles.registrationInput}
-              />
-              {errors.confirmEmail ? (
-                <p classNameName={styles.registrationLabel}>
-                  {errors.confirmEmail}
-                </p>
-              ) : null}
-            </div>
-            <br />
-            <div className='form-group'>
-              <label
-                htmlFor='password'
-                classNameName={styles.registrationLabel}
-              ></label>
-              <input
-                className='form-control'
-                id='password'
-                placeholder='Contraseña'
-                name='password'
-                type='password'
-                value={userData.password}
-                onChange={handleInputChange}
-                classNameName={styles.registrationInput}
-              />
-              {errors.password ? (
-                <p classNameName={styles.registrationLabel}>
-                  {errors.password}
-                </p>
-              ) : null}
-            </div>
-            <br />
-            <div className='form-group'>
-              <label classNameName={styles.registrationLabel}></label>
-              <input
-                name='confirmPassword'
-                placeholder='Confirma Contraseña'
-                type='password'
-                value={userData.confirmPassword}
-                onChange={handleInputChange}
-                classNameName={styles.registrationInput}
-              />
-              {errors.confirmPassword ? (
-                <p classNameName={styles.registrationLabel}>
-                  {errors.confirmPassword}
-                </p>
-              ) : null}
-              <br />
-              <button
-                type='submit'
-                classNameName={`btn btn-primary btn-block mt-4 ${styles.registrationButton}`}
-                onClick={handleClick}
-              >
-                Registrarme
-              </button>
-            </div>
-          </form>
-        </div>
+            <button
+              type='submit'
+              className={`btn btn-primary btn-block mt-4 ${styles.registrationButton}`}
+              onClick={handleClick}
+            >
+              Registrarme
+            </button>
+          </div>
+        </form>
       </div>
       <Footer />
     </div>
