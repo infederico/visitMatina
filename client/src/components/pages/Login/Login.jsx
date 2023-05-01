502fea176f5955f17067e872e4983c3d3e690f01
jose <joseerahe@gmail.com>
2023-04-30 20:28:36 -0600

    punto

--- client/src/components/pages/Login/Login.jsx
+++ client/src/components/pages/Login/Login.jsx
@@ -1,42 +1,42 @@
-import { Link, useNavigate } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import ValidationLogIn from './Validation/validationLogIn'
-import useLocalStorage from '../../localStorage/useLocalStorage'
-import jwt_decode from 'jwt-decode'
-import { useDispatch, useSelector } from 'react-redux'
-import { getUser, authGoogle } from '../../../redux/userActions'
-import style from './login.module.css'
+import { Link, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import ValidationLogIn from './Validation/validationLogIn';
+import useLocalStorage from '../../localStorage/useLocalStorage';
+import jwt_decode from 'jwt-decode';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUser, authGoogle } from '../../../redux/userActions';
+import style from './login.module.css';
 
 const LogIn = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const loggedUser = useSelector((state) => state.user.user)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const loggedUser = useSelector((state) => state.user.user);
   const [userData, setUserData] = useState({
     email: '',
     password: '',
-  })
-  const [rememberButton, setRememberButton] = useState(false)
-  const [remember, setRemember] = useLocalStorage('remember', '')
+  });
+  const [rememberButton, setRememberButton] = useState(false);
+  const [remember, setRemember] = useLocalStorage('remember', '');
   const clientId =
-    '235430067730-qgst6qpfa35tc79r22dh8lqidm9p1qak.apps.googleusercontent.com'
+    '235430067730-qgst6qpfa35tc79r22dh8lqidm9p1qak.apps.googleusercontent.com';
   const [errors, setErrors] = useState({
     email: '',
     password: '',
-  })
+  });
   const handleCallbackResponse = async (response) => {
-    let userObject = jwt_decode(response.credential)
-    dispatch(authGoogle(userObject))
-    document.getElementById('signInDiv').hidden = true
-    navigate('/')
-  }
+    let userObject = jwt_decode(response.credential);
+    dispatch(authGoogle(userObject));
+    document.getElementById('signInDiv').hidden = true;
+    navigate('/');
+  };
 
   useEffect(() => {
     /* global google */
     google.accounts.id.initialize({
       client_id: clientId,
       callback: handleCallbackResponse,
-    })
-    // google.accounts.id.prompt()
+    });
+    google.accounts.id.prompt();
     google.accounts.id.renderButton(document.getElementById('signInDiv'), {
       type: 'standard',
       theme: 'filled_black',
@@ -45,45 +45,45 @@ const LogIn = () => {
       shape: 'rectangle',
       logo_alignment: 'center',
       width: '350',
-    })
+    });
 
     if (remember.email) {
       setUserData((prevState) => ({
         ...prevState,
         email: remember.email,
         password: remember.password,
-      }))
-      setRememberButton(() => true)
+      }));
+      setRememberButton(() => true);
     }
-  }, [remember.email])
+  }, [remember.email]);
 
   const handleInputChange = (event) => {
-    event.preventDefault()
-    setUserData({ ...userData, [event.target.name]: event.target.value })
+    event.preventDefault();
+    setUserData({ ...userData, [event.target.name]: event.target.value });
     setErrors(
       ValidationLogIn({ ...userData, [event.target.name]: event.target.value })
-    )
-  }
+    );
+  };
 
   const handleClick = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     if (Object.keys(errors).length === 0) {
-      dispatch(getUser(userData))
+      dispatch(getUser(userData));
       if (rememberButton) {
         // guardar la información en el almacenamiento local
-        setRemember({ email: userData.email, password: userData.password })
+        setRemember({ email: userData.email, password: userData.password });
       } else {
         // eliminar la información del almacenamiento local
-        setRemember({ email: '', password: '' })
+        setRemember({ email: '', password: '' });
       }
     }
-    console.log('desde loguin', loggedUser)
+    console.log('desde loguin', loggedUser);
 
-    navigate('/')
-  }
+    navigate('/');
+  };
   const handleChecked = () => {
-    rememberButton ? setRememberButton(false) : setRememberButton(true)
-  }
+    rememberButton ? setRememberButton(false) : setRememberButton(true);
+  };
 
   return (
     <div>
@@ -171,16 +171,14 @@ const LogIn = () => {
           <br />
 
           <Link to='/register'>
-            <button
-              style={{ width: '22rem' }}
-            >
+            <button style={{ width: '22rem' }}>
               No tienes cuenta? Registrate
             </button>
           </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LogIn
+export default LogIn;
