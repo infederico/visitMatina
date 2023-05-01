38cbd1b41e3a92d3b9c569d0ec05eb5426735175
jose <joseerahe@gmail.com>
2023-05-01 11:19:34 -0600

    listo para deploy

--- api/index.js
+++ api/index.js
@@ -1,31 +1,29 @@
-const server = require('./src/app.js')
-const { conn } = require('./src/db.js')
-const { swaggerDocs } = require('./src/routes/swagger.js')
-const {seeder,seederReviews,seederProducts} = require('./seeder.js') 
-
-
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const { swaggerDocs } = require('./src/routes/swagger.js');
+const { seeder, seederReviews, seederProducts } = require('./seeder.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(async () => {
+conn.sync({ alter: true }).then(async () => {
   //await saveApi();
-  console.log('Db connected...')
-  async function startServer() {   
+  console.log('Db connected...');
+  async function startServer() {
     try {
       await seeder(); // Llama a la función de seeder
       /* await dos(); */
       console.log('funcion seeder ejecutada..');
       await seederReviews();
       await seederProducts();
-    } catch (error) {   
+    } catch (error) {
       console.error('Error al cargar los datos:', error);
     }
-   
+
     server.listen(3001, () => {
-      console.log('Server listening at 3001') // eslint-disable-line no-console
-      swaggerDocs(server, 3001)
-    })
+      console.log('Server listening at 3001'); // eslint-disable-line no-console
+      swaggerDocs(server, 3001);
+    });
   }
 
   // Llama a la función asíncrona
   startServer();
-})
+});
